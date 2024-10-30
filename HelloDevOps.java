0cd4bbcc3f2e3524c610f208b921b76e17524002
Ubuntu <ubuntu@ip-172-31-25-119.ec2.internal>
2024-10-30 17:48:05 +0000

    Added getGreeting method

--- HelloDevOps.java
+++ HelloDevOps.java
@@ -7,3 +7,13 @@ public class HelloDevOps {
         System.out.println(new HelloDevOps().getMessage());
     }
 }
+
+public class HelloDevOps {
+    public String getMessage() {
+        return "Hello, DevOps!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloDevOps().getMessage());
+    }
+}
