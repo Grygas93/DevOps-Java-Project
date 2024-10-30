fc6711e11c5627294b46cff04e7a66444ce13095
Ubuntu <ubuntu@ip-172-31-25-119.ec2.internal>
2024-10-30 17:42:11 +0000

    Add HelloDevOps program

---
+++ HelloDevOps.java
@@ -0,0 +1,9 @@
+public class HelloDevOps {
+    public String getMessage() {
+        return "Hello, DevOps!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloDevOps().getMessage());
+    }
+}
