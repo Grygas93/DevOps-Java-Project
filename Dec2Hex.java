c38fd1b288ee67184e95e2e688f0d6ceb5628152
Ubuntu <ubuntu@ip-172-31-25-37.ec2.internal>
2024-11-07 19:32:54 +0000

    Updated Dex2Hec

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,5 +1,8 @@
 class Dec2Hex {
     public static String decimalToHex(int num) {
+         if (num == 0) {
+        return "0";
+    }
         char[] ch = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
         String hexadecimal = "";
