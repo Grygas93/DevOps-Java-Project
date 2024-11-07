3c5284a3d3068e84121da65ef05cfadb78bf5532
Ubuntu <ubuntu@ip-172-31-25-37.ec2.internal>
2024-11-07 19:26:30 +0000

    Added error handling for missing and non-integer inputs

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
+class Dec2Hex {
+    public static String decimalToHex(int num) {
+        char[] ch = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem;
+        String hexadecimal = "";
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        // Convert decimal number to hexadecimal
+        while(num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        return hexadecimal;
+    }
 
+    public static void main(String[] args) {
+        // Check if an input argument is provided
+        if (args.length == 0) {
+            System.out.println("Please provide a decimal number as a command-line argument.");
+            return;
+        }
+
+        try {
+            // Try to parse the input argument as an integer
+            int num = Integer.parseInt(args[0]);
+            String hex = decimalToHex(num);
+
+            // Print the hexadecimal result or "0" if the result is empty
+            System.out.println("Hexadecimal representation: " + (hex.isEmpty() ? "0" : hex));
+        } catch (NumberFormatException e) {
+            // Handle non-integer input gracefully by printing an error message
+            System.out.println("Invalid input. Please enter a valid integer.");
+        }
     }
 }
